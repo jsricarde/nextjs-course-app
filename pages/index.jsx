b2505f77469cf541ce7b0c93fce26b4e426ac03f
jsricarde <jsricarde@gmail.com>
2020-10-12 23:49:40 -0500

    NEXT-01: Add code base

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,22 +1,15 @@
-/** @jsx jsx */
-import { jsx } from 'theme-ui'
+import React from 'react';
 import Link from 'next/link'
 
-export default ({content}) => (
-  <div sx={{ height: `calc(100vh - 60px)`}}>
-    <div sx={{variant: 'containers.page', display: 'flex', alignItems: 'center', height: '100%'}}>
-      <h1 sx={{fontSize: 8, my: 0}}>{content.title}</h1>
-    </div>
-  </div> 
+const Page = () => (
+<div>
+    <h1>
+      Index Page
+    </h1>
+    <Link href="/notes">
+      Notes
+    </Link>
+  </div>
 )
 
-
-export async function getStaticProps() {
-  return {
-    props: {
-      content: {
-        title: 'Look at my note app tho'
-      }
-    }
-  }
-}
+export default Page
--- pages/notes/index.jsx
+++ pages/notes/index.jsx
@@ -1,35 +1,23 @@
-/** @jsx jsx */
-import { jsx } from 'theme-ui'
+// pages/notes/index.jsx
+import React from 'react'
 import Link from 'next/link'
 
-export default ({notes}) => {
-  return (
-    <div sx={{variant: 'containers.page'}}>
-      <h1>My Notes</h1>
+export default () => {
+  const notes = new Array(15).fill(1).map((e, i) => ({id: i, title: `Note: ${i}`}))
 
-      <div sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', flexWrap: 'wrap'}}>
-        {notes.map(note => (
-          <div sx={{width: '33%', p: 2}} key={note.id}>
-            <Link href="/notes/[id]" as={`/notes/${note.id}`}>
-              <a sx={{textDecoration: 'none', cursor: 'pointer'}}>
-                <div sx={{variant: 'containers.card',}}>
-                  <strong>{note.title}</strong>
-                </div>
-              </a>
-            </Link>
-          </div>
-        ))}
-      </div>
+  return (
+    <div>
+      <h1>Notes</h1>
+
+      {notes.map(note => (
+        <div key={note.id}>
+          <Link key={note.id} href="/notes/[id]" as={`/notes/${note.id}`}>
+            <a>
+              <strong>{note.title}</strong>
+            </a>
+          </Link>
+        </div>
+      ))}
     </div>
   )
-}
-
-
-export async function getServerSideProps() {
-  const res = await fetch(`http://localhost:3000/api/note`)
-  const {data} = await res.json()
-
-  return {
-    props: {notes: data}
-  }
 }
